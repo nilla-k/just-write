1159ad71ba7c863abbc331784cce6f2cc89d6a77
Nilla Kumaran <nillapria@gmail.com>
2022-07-23 11:25:48 +1000

    config env var and prettier

--- services/index.tsx
+++ services/index.tsx
@@ -1,32 +1,40 @@
-import  { request, gql } from 'graphql-request';
-// Documentation for GraphQL Request: https://www.npmjs.com/package/graphql-request 
+import { request, gql } from 'graphql-request';
+// Documentation for GraphQL Request: https://www.npmjs.com/package/graphql-request
 // Bottom line is that this library will allows us to make GraphQL queries.
 
-// We're making a function and immediately exporting it. 
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+// We're making a function and immediately exporting it.
 export const getPosts = async () => {
-    const query = gql`
+  const query = gql`
     query MyQuery {
-        postsConnection {
-          edges {
-            node {
-              author {
-                name
-                id
-              }
-              createdAt
+      postsConnection {
+        edges {
+          node {
+            author {
+              name
+              id
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            headerImage {
+              url
+            }
+            categories {
+              name
               slug
-              title
-              excerpt
-              headerImage {
-                url
-              }
-              categories {
-                name
-                slug
-              }
             }
           }
         }
       }
-    `
-}
+    }
+  `;
+  if (graphqlAPI === undefined) {
+    throw new TypeError('Missing env variable for NEXT_PUBLIC_GRAPHCMS_ENDPOINT');
+  } else {
+    const result = await request(graphqlAPI, query);
+    return result.postConnection.edges;
+  }
+};
