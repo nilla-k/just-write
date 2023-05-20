a02c496f7dcc924cb77f633d511f434e7a9f3795
Nilla Kumaran <nillapria@gmail.com>
2023-05-20 19:13:49 +1000

    Add key to child elements

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -24,28 +24,28 @@ const PostWidget = ({ categories, slug }: { categories: [Category]; slug: string
       <h3 className="font-semibold  pb-3">{slug ? 'Related posts' : 'Recent posts'}</h3>
       <div>
         {suggestedPosts.map((post: Post) => (
-          <div key={post.title} className="flex items-center w-full mb-5">
-            <div className="w-16 flex-none">
-              <img
-                alt={post.title}
-                className="align-middle rounded-sm object-fill w-16 h-16"
-                src={post.headerImage.url}
-              />
-            </div>
-            <div className="flex-grow ml-4">
-              <p className="text-sm font-semibold">
-                <Link href={`/post/${post.slug}`}>{post.title}</Link>
-              </p>
-              <p className="text-gray-500 text-xs pb-2">
-                {moment(post.createdAt).format('DD MMM, YYYY')}
-              </p>
-              <div className="flex gap-1 flex-wrap">
-                {post.categories.map((category: Category) => (
-                  <CategoryBadge category={category.name} slug={category.slug} />
-                ))}
+          <Link href={`/post/${post.slug}`} key={post.slug}>
+            <div key={post.title} className="flex items-center w-full mb-5">
+              <div className="w-16 flex-none">
+                <img
+                  alt={post.title}
+                  className="align-middle rounded-sm object-fill w-16 h-16"
+                  src={post.headerImage.url}
+                />
+              </div>
+              <div className="flex-grow ml-4">
+                <p className="text-sm font-semibold">{post.title}</p>
+                <p className="text-gray-500 text-xs pb-2">
+                  {moment(post.createdAt).format('DD MMM, YYYY')}
+                </p>
+                <div className="flex gap-1 flex-wrap">
+                  {post.categories.map((category: Category) => (
+                    <CategoryBadge category={category.name} key={category.slug} />
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
