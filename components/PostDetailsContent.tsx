16145bd4b56f72b7c923b225a8bd2c37e975547d
Nilla Kumaran <nillapria@gmail.com>
2023-05-20 19:28:43 +1000

    Move date to right side on large screens

--- components/PostDetailsContent.tsx
+++ components/PostDetailsContent.tsx
@@ -30,13 +30,21 @@ const PostDetailsContent = ({ post }: { post: PostDetails }) => {
         />
       </div>
       <div className="pb-5 pl-5 pr-5 lg:p-0">
-        <h1 className="font-semibold  text-2xl">{`${post.title}`}</h1>
-        <p className="text-gray-700 font-serif">{moment(post.createdAt).format('DD MMM, YYYY')}</p>
-        <p className="text-gray-500 font-serif text-sm italic">
-          {post.createdAt?.includes(post.updatedAt)
-            ? ''
-            : `Updated ${moment(post.updatedAt).format('DD MMM, YYYY')}`}
-        </p>
+        <div className="grid grid-cols-1 xl:grid-cols-3">
+          <div className="xl:col-span-2">
+            <h1 className="font-semibold  text-2xl">{`${post.title}`}</h1>
+          </div>
+          <div className="xl:col-span-1 xl:text-right">
+            <p className="text-gray-700 font-serif">
+              {moment(post.createdAt).format('DD MMM, YYYY')}
+            </p>
+            <p className="text-gray-500 font-serif text-sm italic">
+              {post.createdAt?.includes(post.updatedAt)
+                ? ''
+                : `Updated ${moment(post.updatedAt).format('DD MMM, YYYY')}`}
+            </p>
+          </div>
+        </div>
         <div className="flex gap-1 flex-wrap pt-3">
           {post.categories.map((category: Category) => (
             <CategoryBadge category={category.name} />
